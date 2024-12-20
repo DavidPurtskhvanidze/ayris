6b27366aaba3bafc418d50030b9ecf5de331a212
david <david.purtskhvanidze@gmail.com>
2024-12-20 13:19:24 +0600

    5

---
+++ js/modules/menu.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('.header');
+
+function headerBgColorSwitcher() {
+    if (window.scrollY > 50) {
+        header.classList.add('header_color-active');
+    } else {
+        header.classList.remove('header_color-active');
+    }
+}
+
+window.addEventListener('scroll', headerBgColorSwitcher);
