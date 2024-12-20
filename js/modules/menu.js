643bcdfcd177077c5311a4d9f91119c03865118c
david <david.purtskhvanidze@gmail.com>
2024-12-21 02:57:40 +0600

    7

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -10,3 +10,24 @@ function headerBgColorSwitcher() {
 
 window.addEventListener('DOMContentLoaded', headerBgColorSwitcher);
 window.addEventListener('scroll', headerBgColorSwitcher);
+
+
+const linkPicturePlace = document.getElementById('linkPicturePlace');
+const menuDropLink = document.querySelectorAll('.menu-drop__link');
+menuDropLink.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        if (link.classList.contains('menu-drop__link_underline')) return;
+        const newImage = link.getAttribute('data-menu-drop-image');
+        const newImageSrc = link.getAttribute('data-menu-drop-image');
+
+        linkPicturePlace.innerHTML = `
+            <source srcset="images/pictures/`+ newImage +`.webp" type="image/webp">
+            <source srcset="images/pictures/`+ newImage +`.jpg" type="image/jpeg">
+            <img src="images/pictures/`+ newImage +`.jpg" alt="Picture">
+      `;
+
+        menuDropLink.forEach(link => link.classList.remove('menu-drop__link_underline'));
+        link.classList.add('menu-drop__link_underline');
+    });
+});
+
