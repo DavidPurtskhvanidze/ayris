6ab55dfdcbf53bc68d4b9dde0d7210fe3b901155
david <david.purtskhvanidze@gmail.com>
2024-12-26 02:46:54 +0600

    16

---
+++ js/modules/dropdown-list.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Find all dropdowns on the page
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const button = dropdown.querySelector('.dropdown__button');
+        const list = dropdown.querySelector('.dropdown__list');
+        const items = dropdown.querySelectorAll('.dropdown__list-item');
+
+        // Open/close list by clicking on button
+        button.addEventListener('click', () => {
+            const isOpen = dropdown.classList.contains('open');
+            closeAllDropdowns(); // Закрываем все списки
+            if (!isOpen) {
+                dropdown.classList.add('open');
+                list.style.display = 'flex';
+            }
+        });
+
+        // Selecting an item from a list
+        items.forEach(item => {
+            item.addEventListener('click', (event) => {
+                // Change the button text to the text of the selected element
+                button.textContent = event.target.textContent;
+
+                // Remove the active class from all items
+                items.forEach(i => i.classList.remove('active'));
+
+                // Add the active class to the selected item
+                event.target.classList.add('active');
+
+                // We close the list
+                dropdown.classList.remove('open');
+                list.style.display = 'none';
+            });
+        });
+    });
+
+    // Close all lists when clicking outside the zone
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.dropdown')) {
+            closeAllDropdowns();
+        }
+    });
+
+    // Function to close all dropdowns
+    function closeAllDropdowns() {
+        document.querySelectorAll('.dropdown').forEach(dropdown => {
+            dropdown.classList.remove('open');
+            dropdown.querySelector('.dropdown__list').style.display = 'none';
+        });
+    }
+});
